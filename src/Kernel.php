851367f78a609a97e904cfc544254ac5ba206fa2
elderguardian <soydev@riseup.net>
2023-04-04 20:45:31 +0200

    Added di container

---
+++ src/Kernel.php
@@ -0,0 +1,32 @@
+<?php
+
+class Kernel implements IKernel
+{
+    function get(string $contract)
+    {
+        $mappings = include "mappings.php";
+        $class = $mappings[$contract];
+        $params = $this->getParameters($class);
+        $paramsAsObjects = [];
+
+        foreach ($params as $param)
+            $paramsAsObjects[] = $this->get($param->getType()->getName());
+
+        return new $class(...$paramsAsObjects);
+    }
+
+    function getParameters($class): array
+    {
+        $reflection = new ReflectionClass($class);
+
+        try {
+            $constructor = $reflection->getMethod('__construct');
+            $params = $constructor->getParameters();
+        } catch (ReflectionException $e) {
+            $params = [];
+        }
+
+        return $params;
+    }
+
+}
